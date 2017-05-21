ed899465904dc1f0a2a1c002d6b2b3bf8bf1d031
Mike Limberg <mikelimberg@Mikes-MacBook-Pro.local>
2017-05-21 17:27:06 -0600

    Refactor

--- checks/checkStraight.js
+++ checks/checkStraight.js
@@ -1,30 +1,29 @@
 const checkStraight = (obj) => {
   const keys = Object.keys(obj);
   let straight = false;
+  let lowStraight = false;
 
   if(!obj['5'] && !obj['10'] || obj['5'] && obj['10']) {
     straight = false;
+    lowStraight = false;
+  } else {
+    const aceValue = keys.includes('5') ? 1 : 14;
+    const numberVals = { J: 11, Q: 12, K: 13, A: aceValue };
+
+    const sortedVals = keys.map(val => {
+      return numberVals[val] ? numberVals[val] : parseInt(val);
+    }).sort((a, b) => b - a)
+
+    const highVal = sortedVals[0]
+    const lowVal = sortedVals[sortedVals.length - 1]
+
+    straight = (highVal - lowVal === 4) ? true : false;
+    lowStraight = (aceValue === 1) && straight ? true : false
   }
-
-  const aceValue = keys.includes('5') ? 1 : 14;
-  const numberVals = { J: 11, Q: 12, K: 13, A: aceValue };
-
-  const sortedVals = keys.map(val => {
-    return numberVals[val] ? numberVals[val] : parseInt(val);
-  }).sort((a, b) => b - a)
-
-  const count = sortedVals.reduce((sum, num, i, arr) => {
-    if(i !== arr.length - 1) {
-      sum += num - arr[i + 1];
-    }
-    return sum;
-  }, 0)
-
-  straight = count === 4 ? true : false;
 
   return {
     straight,
-    lowStraight: aceValue === 1 && straight ? true : false
+    lowStraight
   }
 }
 
